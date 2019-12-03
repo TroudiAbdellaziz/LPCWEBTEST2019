f5c635ab248325b8d5862ef17173e80847b74fbe
lepotcommun <30501704+lepotcommun@users.noreply.github.com>
2019-12-03 17:33:13 +0100

    Update app.constants.js

--- app/js/app.constants.js
+++ app/js/app.constants.js
@@ -2,7 +2,7 @@
 
 angular.module('LpcWebTest2018')
   .constant('RESOURCES',(function(){
-    var BASE_URL = 'https://recette-flash.lepotcommuntest.fr/recruting/2018';
+    var BASE_URL = 'https://recette-flash.lepotcommuntest.fr/recruting/2019';
     return {
       PROPERTIES_URL: BASE_URL + '/get-properties',
       GET_POTS_URL: BASE_URL + '/get-pots'
